77850408087ad6f6c5df90aecf485edd6963dc39
lwenjim <779772852@qq.com>
2022-07-03 22:22:42 +0800

    axios post 请求后端数据

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    proxy:{
+      '/apis':{
+        target: 'http://127.0.0.1:8081/',
+        secure:false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/apis': ''
+        }
+      }
+    }
+  }
 })
